fe150b976bbd0a1c52a631dfb6b0f451fe449e7d
ayush166 <ayushkumar3936@gmail.com>
2024-02-11 16:52:29 +0530

    ROUTES

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -6,6 +6,7 @@ import SplashScreen from '../screens/SplashScreen';
 import { useAuthContext } from '../hooks/useAuth';
 import SettingsScreen from '../screens/SettingsScreen';
 import StatsScreen from '../screens/StatsScreen';
+import { ROUTES } from './routes';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,22 +18,19 @@ const AppNavigator = () => {
 
   if (isLoading) {
     // loading indicator
-    screens = <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />;
+    screens = <Stack.Screen name={ROUTES.SPLASH} component={SplashScreen} options={{ headerShown: false }} />;
   } else if (authTokens) {
     // User is signed in, show app screens
     screens = (
       <>
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-        <Stack.Screen name="Stats" component={StatsScreen} />
-       
-       
-       
+        <Stack.Screen name={ROUTES.DASHBOARD} component={DashboardScreen} />
+        <Stack.Screen name={ROUTES.SETTINGS} component={SettingsScreen} />
+        <Stack.Screen name={ROUTES.STATS} component={StatsScreen} />
       </>
     );
   } else {
     // User is not signed in, show sign in screen
-    screens = <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />;
+    screens = <Stack.Screen name={ROUTES.SIGNIN} component={SignInScreen} options={{ headerShown: false }} />;
   }
 
   return (
